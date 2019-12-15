4f43f843fd69cf1fed14a70f98eca85382209eed
Kirill Zorin <zorin.kirill.serg@yandex.ru>
2019-12-16 02:18:10 +0300

    Payconiq Assignment
    Update html page design

--- src/main/resources/static/stocks.js
+++ src/main/resources/static/stocks.js
@@ -2,61 +2,68 @@ $.ajax({
     url: '/api/stocks',
     type: 'GET',
     success: function(data) {
-       var stocksTable = '';
-       $.each(data, function (i, stock) {
-           var removeRow=document.createElement("BUTTON");
-           removeRow.innerHTML= "Update Price";
-           removeRow.onclick=function(){
-               removeRowbyItem(this);
-           };
-           element.type = "text";
-           element.value = stock.currentPrice;
-           element.name = "update_" + stock.id;
-           stocksTable +=
-           '<tr><td>' + stock.id
-           + '</td><td>' + stock.name
-           + '</td><td>' + stock.currentPrice
-           + '</td><td>' + stock.lastUpdate
-           + '</td><td>' + element
-           + '</td></tr>';
-       });
-       $('#stocks_table').append(stocksTable);
+        var table = document.getElementById("stocks_table");
+        var lastId = 0;
+        $.each(data, function (i, stock) {
+            var input_price = document.createElement("input");
+            input_price.type = "text";
+            var update_button = document.createElement("button");
+            update_button.innerHTML = "Update";
+            update_button.onclick=function(){
+                updatePrice(stock.id, input_price.value);
+            };
+            lastId = i + 1;
+            var row = table.insertRow(lastId);
+            var cell1 = row.insertCell(0);
+            var cell2 = row.insertCell(1);
+            var cell3 = row.insertCell(2);
+            var cell4 = row.insertCell(3);
+            var cell5 = row.insertCell(4);
+
+            cell1.innerHTML = stock.id;
+            cell2.innerHTML = stock.name;
+            cell3.innerHTML = stock.currentPrice;
+            cell4.innerHTML = stock.lastUpdate;
+            cell5.appendChild(input_price);
+            cell5.appendChild(update_button);
+        });
+        var input_new_name = document.createElement("input");
+        input_new_name.type = "text";
+        var input_new_price = document.createElement("input");
+        input_new_price.type = "text";
+
+        var add_new_button = document.createElement("button");
+        add_new_button.innerHTML = "+";
+        add_new_button.onclick=function(){
+            addNewStock(input_new_name.value, input_new_price.value);
+        };
+
+        var row = table.insertRow(lastId + 1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        cell1.appendChild(add_new_button)
+        cell2.appendChild(input_new_name);
+        cell3.appendChild(input_new_price);
     }
 });
-$(document).ready(function(){
-        $("#add_stock_submit").on('click', function(){
-            $.ajax({
-                url: '/api/stocks',
-                type : "POST",
-                dataType : 'json',
-                contentType: 'application/json;charset=UTF-8',
-                data :JSON.stringify( { "name": $('#stock_name').val(), "price": $('#stock_price').val() } ),
-                success : function(result) {
-                    console.log(result);
-                },
-                error: function(xhr, resp, text) {
-                    console.log(xhr, resp, text);
-                }
-            })
-        });
-    });
-$(document).ready(function(){
-        $("#update_stock_submit").on('click', function(){
-            $.ajax({
-                url: '/api/stocks/'+$('#update_id').val(),
-                type : "PUT",
-                dataType : 'json',
-                contentType: 'application/json;charset=UTF-8',
-                data :JSON.stringify( { "price": $('#update_price').val() } ),
-                success : function(result) {
-                    console.log(result);
-                },
-                error: function(xhr, resp, text) {
-                    console.log(xhr, resp, text);
-                }
-            })
-        });
-    });
+function addNewStock(name, price){
+    $.ajax({
+        url: '/api/stocks',
+        type : "POST",
+        dataType : 'json',
+        contentType: 'application/json;charset=UTF-8',
+        data :JSON.stringify( { "name": name, "price": price } ),
+        success : function(result) {
+            console.log(result);
+        },
+        error: function(xhr, resp, text) {
+            console.log(xhr, resp, text);
+        }
+    })
+}
 function updatePrice(id, price){
     $.ajax({
         url: '/api/stocks/'+ id,
